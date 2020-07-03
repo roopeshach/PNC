17bd979aeaa6174b230088d72a30c4ad83ed0db4
ubuntu <rupeshacharya2014@gmail.com>
2020-07-03 15:11:47 +0545

    All news and notices added

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,6 @@ from news import views
 urlpatterns = [
     
     path('news/<str:slug>', views.viewNews , name="news" ),
+    path('news/',views.allNews, name="allNews"),
+    path('notice/',views.allNotice, name="allNotice")
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from news.models import News
+from news.models import News , Notice
 from category.models import Department, Program , Custom_Page
 from home.models import Content
+from django.core.paginator import Paginator ,  EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
@@ -23,3 +24,54 @@ def viewNews(request, slug):
 
     }
     return render(request , 'news/news.html' , context_news)
+
+def allNews(request):
+
+    news_list = News.objects.all()
+
+
+    page = request.GET.get('page', 1)    
+    paginator = Paginator(news_list, 8)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+
+
+    context = {
+        'content' : content,
+        'departments' : departments,
+        'programs' : programs,
+        'pages' : pages,
+        'news' : news,
+    }
+    print(news)
+    return render(request , 'news/allnews.html', context)
+
+
+def allNotice(request):
+
+    notice_list = Notice.objects.all()
+
+
+    page = request.GET.get('page', 1)    
+    paginator = Paginator(notice_list, 8)
+    try:
+        notice = paginator.page(page)
+    except PageNotAnInteger:
+        notice = paginator.page(1)
+    except EmptyPage:
+        notice = paginator.page(paginator.num_pages)
+
+
+    context = {
+        'content' : content,
+        'departments' : departments,
+        'programs' : programs,
+        'pages' : pages,
+        'notice' : notice,
+    }
+    print(notice)
+    return render(request , 'news/allnotice.html', context)
